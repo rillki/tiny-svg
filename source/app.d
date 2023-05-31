b9d7f033ac8ba3cb4b3e3beb3cf6db1de0edfe53
Ki Rill <rill.ki@yahoo.com>
2023-05-31 18:20:08 +0600

    update

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
-import std.stdio;
+module app;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+void main() {
+	import tiny_svg;
+
+    SVGCanvas canvas = SVGCanvas(512, 512);
+    
+    // draw
+    canvas.drawRectangle(384, 384, 64, 64, "#00FF00", "#000000", 4, 0, 0);
+    canvas.drawRectangle(320, 320, 96, 96, "#FFFF00", "#000000", 2, 8, 8);
+    canvas.drawRectangle(256, 256, 128, 128, "#00FFFF", "#000000", 2, 8, 8);
+    canvas.drawRectangle(192, 192, 160, 160, "#FF80FF", "#FF80FF", 2, -10, -1000);
+    
+    canvas.save("test.svg");
 }
+
+
---
+++ source/tiny_svg.d
@@ -0,0 +1,43 @@
+module tiny_svg;
+
+import std.string: format;
+
+struct SVGCanvas {
+    private uint width, height;
+    private string content;
+
+    this(in uint width, in uint height) {
+        this.width = width;
+        this.height = height;
+
+        content = (
+            "<svg width='%s px' height='%s px' xmlns='%s' version='%s' xmlns:xlink='%s'>\n"
+        ).format(
+            width, height, "http://www.w3.org/2000/svg", "1.1", "http://www.w3.org/1999/xlink"
+        );
+    }
+
+    void drawRectangle(
+        in int width, in int height, 
+        in int x, in int y, 
+        in string fill, in string stroke, 
+        in int strokewidth, in int radiusx, in int radiusy
+    ) {
+        content ~= (
+            "<rect fill='%s' stroke='%s' " ~
+            "stroke-width='%s px' width='%s' height='%s' " ~
+            "y='%s' x='%s' ry='%s' rx='%s'/>\n"
+        ).format(
+            fill, stroke, 
+            strokewidth, width, height,
+            y, x, radiusy, radiusx
+        );
+    }
+
+    void save(in string name) {
+        import std.stdio: File;
+        File(name, "w").write(content ~ "</svg>");
+    }
+}
+
+
