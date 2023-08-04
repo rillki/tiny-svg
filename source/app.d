efe968409a443634d8dbecb11e8fb6bf2ba4012a
Ki Rill <rill.ki@yahoo.com>
2023-08-04 18:28:54 +0600

    README update

--- source/app.d
+++ source/app.d
@@ -146,7 +146,7 @@ void test_example()
 {
     SVGCanvas canvas = SVGCanvas(240, 240);
 
-    // add gradient
+    // create a radial gradient
     new RadialGradient("rg0", Colors.gold, Colors.orange)
         .setOpacityA(0.4)
         .setOpacityB(0.9)
@@ -156,6 +156,7 @@ void test_example()
     new Rectangle(0, 0, canvas.width, canvas.height)
         .setStrokeColor(Colors.blue)
         .setStrokeWidth(0)
+        .setRadius(24)
         .setGradient("rg0")
         .addToCanvas(canvas);
     
